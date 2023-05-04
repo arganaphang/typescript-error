0273357f6ba81b6abbbcd76612f396ca7726a60e
Argana Phangquestian <arganaphangquestian@gmail.com>
2023-05-04 20:54:50 +0700

    chore: update file structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 // Reference: https://kkalamarski.me/a-declarative-approach-to-error-handling-in-typescript
-import Result, { Err, Ok } from "./err";
+// Magic start here
+export default interface Result<Ok, Err> {
+  match<O>(x: { Ok: (v: Ok) => O; Err: (v: Err) => O }): O;
+}
+
+export const Err = <Ok, Err>(value: Err): Result<Ok, Err> => ({
+  match: ({ Err }) => Err(value),
+});
+
+export const Ok = <Ok, Err>(value: Ok): Result<Ok, Err> => ({
+  match: ({ Ok }) => Ok(value),
+});
+// Magic end here
 
 type User = {
   name: string;
